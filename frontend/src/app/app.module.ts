d2977cd057fed5144d5ad64dc20e15159f8189a4
nikit <nikita.kuzmichenko@gmai.com>
2022-11-12 23:19:19 +0300

    pages laid out

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/page/login/login/login.component';
+import { SensorTableComponent } from './component/page/sensorTable/sensor-table/sensor-table.component';
+import { SensorEditorComponent } from './component/page/sensorEditor/sensor-editor/sensor-editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SensorTableComponent,
+    SensorEditorComponent
   ],
   imports: [
     BrowserModule
---
+++ frontend/src/app/component/page/sensorEditor/sensor-editor/sensor-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sensor-editor',
+  templateUrl: './sensor-editor.component.html',
+  styleUrls: ['./sensor-editor.component.scss']
+})
+export class SensorEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
