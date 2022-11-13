c0520e4b60e3f71405915d16eee3fe7111978350
nikit <nikita.kuzmichenko@gmai.com>
2022-11-13 10:13:18 +0300

    routing added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule,Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './component/page/login/login/login.component';
 import { SensorTableComponent } from './component/page/sensorTable/sensor-table/sensor-table.component';
 import { SensorEditorComponent } from './component/page/sensorEditor/sensor-editor/sensor-editor.component';
+
+const appRoutes: Routes =[
+  {
+    path :'login' ,
+    component : LoginComponent
+  },
+  { 
+    path: '', 
+    redirectTo: '/login', 
+    pathMatch: 'full' 
+  },
+  {
+    path :'sensors/create' ,
+    component : SensorEditorComponent
+  },
+  {
+    path :'sensors/edit' ,
+    component : SensorEditorComponent
+  },
+  {
+    path :'sensors' ,
+    component : SensorTableComponent
+  }
+]
 
 @NgModule({
   declarations: [
@@ -14,6 +39,7 @@ import { SensorEditorComponent } from './component/page/sensorEditor/sensor-edit
     SensorEditorComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
   providers: [],
